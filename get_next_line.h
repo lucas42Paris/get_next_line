3bbf7645b9185584ff9cc5a8284746e6ae48dfe3
Ontiimeyes <lucassalinroge@gmail.com>
2023-07-10 15:38:56 +0200

    Ready to push

---
+++ get_next_line.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsalin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/10 15:19:52 by lsalin            #+#    #+#             */
+/*   Updated: 2022/08/10 15:20:25 by lsalin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE BUFSIZ
+# endif
+
+# if BUFFER_SIZE > 9223372036854775806
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 0
+# endif
+
+char	*get_next_line(int fd);
+void	ft_read_line(int fd, char **keep, char **tmp);
+char	*ft_parse_line(char **keep, char **tmp);
+char	*get_before_newline(const char *s);
+char	*get_after_newline(const char *s);
+int		contains_newline(const char *s);
+char	*ft_strdup(const char *s1);
+char	*join_strs(const char *s1, const char *s2);
+void	*ft_malloc_zero(size_t count, size_t size);
+void	ft_free_strs(char **str, char **str2, char **str3);
+
+#endif
