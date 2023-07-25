8ef8726828b9bf31de828c3fb80d5729ae2c1c87
Lucas <lucas@MacBook-Air-de-Lucas.local>
2023-07-25 19:39:23 +0200

    Ready to push

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "get_next_line.h"
+
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	(void)argc;
+	fd = open(argv[1], O_RDONLY);
+	line = "";
+	while (line != NULL)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+	}
+	fd = close(fd);
+	return (0);
+}
